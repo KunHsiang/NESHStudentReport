77989bf4da128dbe04a81d24b83a69ed197d8ab8
KunHsiang Chang <web@kunhsiang.com>
2014-07-25 12:15:20 +0800

    加入初始化專案

---
+++ NESHStudentReport/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+using NESHStudentReport.Properties;
+
+namespace NESHStudentReport
+{
+    public class Program
+    {
+        [FISCA.MainMethod()]
+        public static void Main()
+        {
+            //學生->資料統計->報表->成績相關報表->學生成績通知單(3~6年級)
+            K12.Presentation.NLDPanels.Student.RibbonBarItems["資料統計"]["報表"]["成績相關報表"]["學生成績通知單(3~6年級)"].Click += (sender, e) => new frmHome("學生成績通知單(3~6年級)",Resources.學生成績通知單_3_6).ShowDialog();
+            K12.Presentation.NLDPanels.Student.RibbonBarItems["資料統計"]["報表"]["成績相關報表"]["學生成績通知單(7~8年級)"].Click += (sender, e) => new frmHome("學生成績通知單(7~8年級)",Resources.學生成績通知單_7_8).ShowDialog();
+            K12.Presentation.NLDPanels.Student.RibbonBarItems["資料統計"]["報表"]["成績相關報表"]["學生成績通知單(9~12年級)"].Click += (sender, e) => new frmHome("學生成績通知單(9~12年級)",Resources.學生成績通知單_9_12).ShowDialog();
+        }
+    }
+}
---
+++ NESHStudentReport/frmHome.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace NESHStudentReport
+{
+    partial class frmHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbSchoolYear = new DevComponents.DotNetBar.Controls.ComboBoxEx();
+            this.labelX1 = new DevComponents.DotNetBar.LabelX();
+            this.circularProgress = new DevComponents.DotNetBar.Controls.CircularProgress();
+            this.btnPrint = new DevComponents.DotNetBar.ButtonX();
+            this.btnExit = new DevComponents.DotNetBar.ButtonX();
+            this.SuspendLayout();
+            // 
+            // cmbSchoolYear
+            // 
+            this.cmbSchoolYear.DisplayMember = "Text";
+            this.cmbSchoolYear.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.cmbSchoolYear.FormattingEnabled = true;
+            this.cmbSchoolYear.ItemHeight = 19;
+            this.cmbSchoolYear.Location = new System.Drawing.Point(102, 21);
+            this.cmbSchoolYear.Name = "cmbSchoolYear";
+            this.cmbSchoolYear.Size = new System.Drawing.Size(164, 25);
+            this.cmbSchoolYear.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.cmbSchoolYear.TabIndex = 1;
+            // 
+            // labelX1
+            // 
+            this.labelX1.AutoSize = true;
+            this.labelX1.BackColor = System.Drawing.Color.Transparent;
+            // 
+            // 
+            // 
+            this.labelX1.BackgroundStyle.Class = "";
+            this.labelX1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labelX1.Location = new System.Drawing.Point(22, 20);
+            this.labelX1.Name = "labelX1";
+            this.labelX1.Size = new System.Drawing.Size(74, 26);
+            this.labelX1.TabIndex = 2;
+            this.labelX1.Text = "學年度：";
+            this.labelX1.Click += new System.EventHandler(this.labelX1_Click);
+            // 
+            // circularProgress
+            // 
+            this.circularProgress.BackColor = System.Drawing.Color.Transparent;
+            // 
+            // 
+            // 
+            this.circularProgress.BackgroundStyle.Class = "";
+            this.circularProgress.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.circularProgress.Location = new System.Drawing.Point(22, 67);
+            this.circularProgress.Name = "circularProgress";
+            this.circularProgress.Size = new System.Drawing.Size(46, 28);
+            this.circularProgress.Style = DevComponents.DotNetBar.eDotNetBarStyle.OfficeXP;
+            this.circularProgress.TabIndex = 87;
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btnPrint.AutoExpandOnClick = true;
+            this.btnPrint.BackColor = System.Drawing.Color.Transparent;
+            this.btnPrint.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.btnPrint.Location = new System.Drawing.Point(102, 67);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(72, 28);
+            this.btnPrint.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.btnPrint.TabIndex = 86;
+            this.btnPrint.Text = "列  印";
+            this.btnPrint.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btnExit.BackColor = System.Drawing.Color.Transparent;
+            this.btnExit.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.btnExit.Location = new System.Drawing.Point(194, 67);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(72, 28);
+            this.btnExit.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.btnExit.TabIndex = 85;
+            this.btnExit.Text = "離  開";
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(287, 113);
+            this.Controls.Add(this.circularProgress);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.labelX1);
+            this.Controls.Add(this.cmbSchoolYear);
+            this.MaximumSize = new System.Drawing.Size(295, 140);
+            this.MinimumSize = new System.Drawing.Size(295, 140);
+            this.Name = "frmHome";
+            this.Text = "學生成績通知單";
+            this.TitleText = "學生成績通知單";
+            this.Load += new System.EventHandler(this.frmHome_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevComponents.DotNetBar.Controls.ComboBoxEx cmbSchoolYear;
+        private DevComponents.DotNetBar.LabelX labelX1;
+        private DevComponents.DotNetBar.Controls.CircularProgress circularProgress;
+        private DevComponents.DotNetBar.ButtonX btnPrint;
+        private DevComponents.DotNetBar.ButtonX btnExit;
+    }
+}
